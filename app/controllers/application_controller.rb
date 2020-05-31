9806779daf5e8933c72d8794e5529565d90e806b
Megan Comeau <mlcomeau99@gmail.com>
2020-05-31 21:08:10 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,39 +9,38 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/articles' do
-    @articles = Article.all 
-    erb :index 
+    @articles = Article.all
+    erb :index
   end
 
-  get '/articles/new' do 
-    erb :new 
-  end 
+  get '/articles/new' do
+    erb :new
+  end
 
-  post '/articles' do 
+  post '/articles' do
     @article = Article.create(:title => params[:title], :content => params[:content])
     redirect to "/articles/#{@article.id}"
-  end 
+  end
 
-    
-  get '/articles/:id' do 
+
+  get '/articles/:id' do
     @article = Article.find_by_id(params[:id])
-    erb :show 
-  end 
+    erb :show
+  end
 
-  get '/articles/:id/edit' do 
+  get '/articles/:id/edit' do
     @article = Article.find_by_id(params[:id])
-    erb :edit 
-  end 
+    erb :edit
+  end
 
-  patch '/articles/:id' do 
+  patch '/articles/:id' do
     @article = Article.find(params[:id])
     @article.update(params[:article])
     redirect to "/articles/#{ @article.id }"
-  end 
+  end
 
   delete "/articles/:id" do
-    @article = Article.find(params[:id])
-    @article.destroy
+    Article.delete(params[:id])
     redirect "/articles"
   end
 
